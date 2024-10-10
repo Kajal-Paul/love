37e0b26a61bdb67d804b86fcd60b8474ecbb8a4b
Kajal-Paul <paulkajal8332@gmail.com>
2024-10-10 12:39:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", () => {
+  const xPos = event.clientX;
+  const yPos = event.clientY;
+ 
+
+  const spanEl = document.createElement("span");
+  spanEl.style.position = "absolute";
+  spanEl.style.left= xPos +"px";
+  spanEl.style.top= yPos +"px";
+  const randHight = Math.random()*100;
+  const randWidth = randHight*1.1;
+  spanEl.style.width =randWidth +"px";
+  spanEl.style.height =randHight+ 'px';
+  const randColor =  Math.random()*1000 +'deg';
+  spanEl.style.filter= `hue-rotate(${randColor})`;
+
+  
+  bodyEl.appendChild(spanEl);
+  setTimeout(()=>{
+    spanEl.remove()
+  },1500)
+
+});
+
+
